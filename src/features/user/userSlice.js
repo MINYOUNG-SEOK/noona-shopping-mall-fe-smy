3e358e504961feee685ed5fc6df547f28c3840e6
Rosie Seok <lovelyrosie731@gmail.com>
2024-10-24 22:36:59 +0900

    userSlice.js

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,7 +6,19 @@ import { initialCart } from "../cart/cartSlice";
 
 export const loginWithEmail = createAsyncThunk(
   "user/loginWithEmail",
-  async ({ email, password }, { rejectWithValue }) => {}
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await api.post("/auth/login", { email, password });
+      // 성공
+      // Login page
+
+      return response.data;
+    } catch (error) {
+      // 실패
+      // 실패 시 생긴 에러 값을 reducer에 저장하기
+      return rejectWithValue(error.error);
+    }
+  }
 );
 
 export const loginWithGoogle = createAsyncThunk(
@@ -15,12 +27,40 @@ export const loginWithGoogle = createAsyncThunk(
 );
 
 export const logout = () => (dispatch) => {};
+
 export const registerUser = createAsyncThunk(
   "user/registerUser",
   async (
     { email, name, password, navigate },
     { dispatch, rejectWithValue }
-  ) => {}
+  ) => {
+    try {
+      const response = await api.post("/user", { email, name, password });
+      // 성공
+      // 1. 성공 토스트 메세지 보여주기
+      dispatch(
+        showToastMessage({
+          message: "회원가입을 성공했습니다.",
+          status: "success",
+        })
+      );
+      // 2. 로그인 페이지로 리다이렉트
+      navigate("/login");
+
+      return response.data.data;
+    } catch (error) {
+      // 실패
+      // 1. 실패 토스트 메세지 보여주기
+      dispatch(
+        showToastMessage({
+          message: "회원가입을 실패했습니다.",
+          status: "error",
+        })
+      );
+      // 2. 에러 값을 저장한다
+      return rejectWithValue(error.error);
+    }
+  }
 );
 
 export const loginWithToken = createAsyncThunk(
@@ -43,7 +83,33 @@ const userSlice = createSlice({
       state.registrationError = null;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(registerUser.fulfilled, (state) => {
+        state.loading = false;
+        state.registrationError = null;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.registrationError = action.payload;
+      })
+      .addCase(loginWithEmail.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(loginWithEmail.fulfilled, (state, action) => {
+        state.loading = false;
+        console.log("Payload received:", action.payload);
+
+        state.user = action.payload.user;
+        state.loginError = null;
+      })
+      .addCase(loginWithEmail.rejected, (state, action) => {
+        state.loading = false;
+        state.loginError = action.payload;
+      });
+  },
 });
 export const { clearErrors } = userSlice.actions;
 export default userSlice.reducer;
