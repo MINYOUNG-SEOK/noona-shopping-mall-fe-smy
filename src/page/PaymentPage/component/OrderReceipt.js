95498f243fa7c157ad4546eba131807210b471d2
Rosie Seok <lovelyrosie731@gmail.com>
2024-11-03 00:44:55 +0900

    [css] paymentpage

--- src/page/PaymentPage/component/OrderReceipt.js
+++ src/page/PaymentPage/component/OrderReceipt.js
@@ -4,47 +4,57 @@ import { useNavigate } from "react-router";
 import { useLocation } from "react-router-dom";
 import { currencyFormat } from "../../../utils/number";
 
-const OrderReceipt = () => {
+const OrderReceipt = ({ selectedItems, totalSelectedPrice }) => {
   const location = useLocation();
   const navigate = useNavigate();
 
   return (
     <div className="receipt-container">
-      <h3 className="receipt-title">주문 내역</h3>
-      <ul className="receipt-list">
-        <li>
-          <div className="display-flex space-between">
-            <div>아이템이름</div>
-
-            <div>₩ 45,000</div>
+      <div className="price-summary">
+        <div className="summary-row">
+          <div className="summary-header">
+            <span>총 주문금액</span>
+            <div className="price-count">
+              <span>{currencyFormat(totalSelectedPrice)}원</span>
+              <span className="count-text">
+                총 {selectedItems?.length || 0}개
+              </span>
+            </div>
           </div>
-        </li>
-      </ul>
-      <div className="display-flex space-between receipt-title">
-        <div>
-          <strong>Total:</strong>
-        </div>
-        <div>
-          <strong>₩ 최종가격</strong>
+
+          <div className="operator-circle">+</div>
+
+          <div className="summary-header">
+            <span>총 배송비</span>
+            <span>0원</span>
+          </div>
+
+          <div className="operator-circle">=</div>
+
+          <div className="summary-header">
+            <span>총 결제금액</span>
+            <span className="total-selected-price">
+              {currencyFormat(totalSelectedPrice)}원
+            </span>
+          </div>
         </div>
       </div>
-      {/* {location.pathname.includes("/cart") && cartList.length > 0 && (
+
+      <div className="button-container">
+        <Button
+          variant="outline-dark"
+          className="continue-shopping-btn"
+          onClick={() => navigate("/")}
+        >
+          CONTINUE SHOPPING
+        </Button>
         <Button
           variant="dark"
-          className="payment-button"
+          className="checkout-btn"
           onClick={() => navigate("/payment")}
         >
-          결제 계속하기
+          CHECK OUT
         </Button>
-      )} */}
-
-      <div>
-        가능한 결제 수단 귀하가 결제 단계에 도달할 때까지 가격 및 배송료는
-        확인되지 않습니다.
-        <div>
-          30일의 반품 가능 기간, 반품 수수료 및 미수취시 발생하는 추가 배송 요금
-          읽어보기 반품 및 환불
-        </div>
       </div>
     </div>
   );
