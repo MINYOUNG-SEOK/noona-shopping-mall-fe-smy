00a94c4982240150bb72613c8b44d5005857178e
Rosie Seok <lovelyrosie731@gmail.com>
2024-11-04 22:06:15 +0900

    [style]paymentPage

--- src/page/PaymentPage/PaymentPage.js
+++ src/page/PaymentPage/PaymentPage.js
@@ -98,107 +98,109 @@ const PaymentPage = () => {
 
   return (
     <Container className="payment-page-container mt-4">
-      <Row className="justify-content-center">
-        <Col lg={8} md={12}>
-          <Form onSubmit={handleSubmit}>
-            {/* 배송 정보, 상품 정보, 결제 정보 */}
+      <Form onSubmit={handleSubmit}>
+        <Row className="justify-content-center">
+          <Col lg={8} md={12}>
             <div className="info-sections">
-            <div className="shipping-info">
-              <div className="shipping-info-title">
-                <h2 className="shipping-title">배송 정보</h2>
-                <p className="required-note">*표시는 필수입력 항목</p>
+              <div className="shipping-info">
+                <div className="shipping-info-title">
+                  <h2 className="shipping-title">배송 정보</h2>
+                  <p className="required-note">*표시는 필수입력 항목</p>
                 </div>
-                {/* 배송 정보 입력 폼 */}
                 <Row className="mb-3">
-                <Form.Group as={Col} controlId="lastName">
-                  
-                <Form.Label className="form-label-required">
-                  성<span className="required-asterisk">*</span>
-                </Form.Label>
-                <Form.Control
-                  type="text"
-                  onChange={handleFormChange}
-                  required
-                  name="lastName"
-                />
-              </Form.Group>
+                  <Form.Group as={Col} controlId="lastName">
+                    <Form.Label className="form-label-required">
+                      성<span className="required-asterisk">*</span>
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      onChange={handleFormChange}
+                      required
+                      name="lastName"
+                    />
+                  </Form.Group>
 
-              <Form.Group as={Col} controlId="firstName">
-                <Form.Label className="form-label-required">
-                  이름<span className="required-asterisk">*</span>
-                </Form.Label>
-                <Form.Control
-                  type="text"
-                  onChange={handleFormChange}
-                  required
-                  name="firstName"
-                />
-              </Form.Group>
-            </Row>
+                  <Form.Group as={Col} controlId="firstName">
+                    <Form.Label className="form-label-required">
+                      이름<span className="required-asterisk">*</span>
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      onChange={handleFormChange}
+                      required
+                      name="firstName"
+                    />
+                  </Form.Group>
+                </Row>
 
-            <Form.Group className="mb-3" controlId="formGridAddress1">
-              <Form.Label className="form-label-required">
-                연락처<span className="required-asterisk">*</span>
-              </Form.Label>
-              <Form.Control
-                placeholder="010-xxx-xxxxx"
-                onChange={handleFormChange}
-                required
-                name="contact"
-              />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formGridAddress1">
+                  <Form.Label className="form-label-required">
+                    연락처<span className="required-asterisk">*</span>
+                  </Form.Label>
+                  <Form.Control
+                    placeholder="010-xxxx-xxxx"
+                    onChange={handleFormChange}
+                    required
+                    name="contact"
+                  />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formGridAddress2">
-              <Form.Label className="form-label-required">
-                주소<span className="required-asterisk">*</span>
-              </Form.Label>
-              <Form.Control
-                placeholder="Apartment, studio, or floor"
-                onChange={handleFormChange}
-                required
-                name="address"
-              />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formGridAddress2">
+                  <Form.Label className="form-label-required">
+                    주소<span className="required-asterisk">*</span>
+                  </Form.Label>
+                  <Form.Control
+                    placeholder="Apartment, studio, or floor"
+                    onChange={handleFormChange}
+                    required
+                    name="address"
+                  />
+                </Form.Group>
 
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label className="form-label-required">
-                  City<span className="required-asterisk">*</span>
-                </Form.Label>
-                <Form.Control
-                  onChange={handleFormChange}
-                  required
-                  name="city"
-                />
-              </Form.Group>
+                <Row className="mb-3">
+                  <Form.Group as={Col} controlId="formGridCity">
+                    <Form.Label className="form-label-required">
+                      City<span className="required-asterisk">*</span>
+                    </Form.Label>
+                    <Form.Control
+                      onChange={handleFormChange}
+                      required
+                      name="city"
+                    />
+                  </Form.Group>
 
-              <Form.Group as={Col} controlId="formGridZip">
-                <Form.Label className="form-label-required">
-                  Zip<span className="required-asterisk">*</span>
-                </Form.Label>
-                <Form.Control onChange={handleFormChange} required name="zip" />
-              </Form.Group>
-            </Row>
+                  <Form.Group as={Col} controlId="formGridZip">
+                    <Form.Label className="form-label-required">
+                      Zip<span className="required-asterisk">*</span>
+                    </Form.Label>
+                    <Form.Control
+                      onChange={handleFormChange}
+                      required
+                      name="zip"
+                    />
+                  </Form.Group>
+                </Row>
 
-            <Form.Group className="mb-3 delivery-message">
-              <Form.Label className="form-label-required">
-                배송 메시지
-              </Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                placeholder="배송 시 요청 사항을 입력해 주세요"
-                onChange={handleFormChange}
-                name="deliveryMessage"
-                value={shipInfo.deliveryMessage}
-              />
-            </Form.Group>
-
+                <Form.Group className="mb-3 delivery-message">
+                  <Form.Label className="form-label-required">
+                    배송 메시지
+                  </Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    placeholder="배송 시 요청 사항을 입력해 주세요"
+                    onChange={handleFormChange}
+                    name="deliveryMessage"
+                    value={shipInfo.deliveryMessage}
+                  />
+                </Form.Group>
               </div>
-              
 
               <div className="selected-items-section">
-                <h2 className="shipping-title">상품 정보 / 총 {selectedItems.length}개</h2>
+                <h2 className="shipping-title">
+                  상품 정보 / 총{" "}
+                  {selectedItems.reduce((total, item) => total + item.qty, 0)}개
+                </h2>
                 {selectedItems.map((item) => (
                   <div key={item.productId._id} className="selected-item mb-3">
                     <div className="selected-item-container">
@@ -208,10 +210,15 @@ const PaymentPage = () => {
                         className="selected-item-image"
                       />
                       <div className="selected-item-details">
-                        <h5 className="selected-item-name">{item.productId.name}</h5>
-                        <p className="selected-item-option">옵션: [SIZE] {item.size}</p>
+                        <h5 className="selected-item-name">
+                          {item.productId.name}
+                        </h5>
+                        <p className="selected-item-option">
+                          옵션: [SIZE] {item.size}
+                        </p>
                         <p className="selected-item-price">
-                          {item.qty}개 / ₩ {currencyFormat(item.productId.price * item.qty)}
+                          {item.qty}개 / ₩{" "}
+                          {currencyFormat(item.productId.price * item.qty)}
                         </p>
                       </div>
                     </div>
@@ -228,35 +235,40 @@ const PaymentPage = () => {
                 />
               </div>
             </div>
-          </Form>
-        </Col>
+          </Col>
 
-        {/* 고정된 주문 요약 영역 */}
-        <Col lg={4} md={12} className="sticky-summary">
-          <div className="receipt-and-button-area">
-            <div className="order-summary">
-              <h3 className="summary-title">결제금액</h3>
-              <div className="summary-item">
-                <span>총 상품 금액</span>
-                <span>₩{currencyFormat(totalPrice)}</span>
+          <Col lg={4} md={12} className="sticky-summary">
+            <div className="receipt-and-button-area">
+              <div className="order-summary">
+                <h3 className="summary-title">결제금액</h3>
+                <div className="summary-item">
+                  <span>총 상품 금액</span>
+                  <span>₩{currencyFormat(totalPrice)}</span>
+                </div>
+                <div className="summary-item">
+                  <span>배송비</span>
+                  <span>무료</span>
+                </div>
+                <div className="summary-item total">
+                  <span>총 결제 금액</span>
+                  <span className="total-amount">
+                    ₩{currencyFormat(totalPrice)}
+                  </span>
+                </div>
               </div>
-              <div className="summary-item">
-                <span>배송비</span>
-                <span>무료</span>
-              </div>
-              <div className="summary-item total">
-                <span>총 결제 금액</span>
-                <span className="total-amount">₩{currencyFormat(totalPrice)}</span>
+              <div className="checkout-button-container">
+                <Button
+                  variant="dark"
+                  type="submit"
+                  className="checkout-button"
+                >
+                  CHECK OUT
+                </Button>
               </div>
             </div>
-            <div className="checkout-button-container">
-            <Button variant="dark" className="checkout-button">
-              CHECK OUT
-            </Button>
-            </div>
-          </div>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      </Form>
     </Container>
   );
 };
